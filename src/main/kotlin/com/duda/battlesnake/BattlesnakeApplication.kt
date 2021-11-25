07ed649f7beb66e8137dca749ded03002a5d6d91
Liat Grinshpun <liat.grinshpun@duda.co>
2021-11-25 14:39:42 +0200

    create project

---
+++ src/main/kotlin/com/duda/battlesnake/BattlesnakeApplication.kt
@@ -0,0 +1,11 @@
+package com.duda.battlesnake
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BattlesnakeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BattlesnakeApplication>(*args)
+}
---
+++ src/test/kotlin/com/duda/battlesnake/BattlesnakeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.duda.battlesnake
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BattlesnakeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
